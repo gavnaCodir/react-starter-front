9d38daaeaae5c06f02fce665d6408d9b9f5b07a5
Mark <markbatin2017@gmail.com>
2019-03-30 20:30:40 +0300

    react starter v2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render(){
       return(
           <div className="App">
-              <h1>React starter</h1>
+              <h1>React starter!</h1>
               <img className="logo" src={logo} alt='...'/>
           </div>
       )
